4b72ff9001693156c6575b8492dfe5d07fe35d67
rindo57 <77073061+rindo57@users.noreply.github.com>
2025-01-25 22:36:23 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,12 +3,13 @@
 from os import getenv
 
 from mongoengine import connect
-
+import pymongo
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 from config import backend_ip
 
 print(f'Starting Engine in {getenv("ENVIRONMENT")}')
 
-MONGO_DATABASE_URL = f"mongodb://user:password@{backend_ip}:27017/" \
+MONGO_DATABASE_URL = f"mongodb+srv://anidl:encodes@cluster0.oobfx33.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" \
     if getenv('ENVIRONMENT') == 'dev' else \
     "mongodb://user:password@localhost:27017/"
 MONGO_DATABASE = "shop"
@@ -16,8 +17,8 @@ MONGO_DATABASE = "shop"
 # client = AsyncIOMotorClient(MONGO_DATABASE_URL)
 # print('Engine Started')
 # engine = AIOEngine(motor_client=client, database=MONGO_DATABASE)
-
-db = connect(host=f'{MONGO_DATABASE_URL}{MONGO_DATABASE}?authSource=admin')
+mongo_client = MongoClient(MONGO_DATABASE_URL)
+db = mongo_client.shop
 
 # Base.metadata.create_all(bind=eng)
 
